a5e66edf20650ef5d9cc8e40acfe86389a59dcec
Denys Potapov <denys.potapov@gmail.com>
2014-10-10 17:06:27 +0300

    fix Ru tests

--- tests/WordList/RuTest.php
+++ tests/WordList/RuTest.php
@@ -6,9 +6,27 @@ use Barzo\Password\WordList;
 
 class RuTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetLast()
+    public function testRu()
     {
         $wordList = new WordList\Ru();
+        $this->assertEquals('посол', $wordList->get(1));
+    }
+
+    public function testRuNouns()
+    {
+        $wordList = new WordList\Ru\Nouns();
         $this->assertEquals('игла', $wordList->get(1));
+    }
+
+    public function testRuVerbs()
+    {
+        $wordList = new WordList\Ru\Verbs();
+        $this->assertEquals('взломать', $wordList->get(1));
+    }
+
+    public function testRuAdjectives()
+    {
+        $wordList = new WordList\Ru\Adjectives();
+        $this->assertEquals('амурный', $wordList->get(1));
     }
 }
