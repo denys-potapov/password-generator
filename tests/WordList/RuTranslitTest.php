6dc0c24ba9e40a35b2d541b2c2d4f8c3c0f3df45
Denys Potapov <denys.potapov@gmail.com>
2014-10-10 17:10:49 +0300

    update RuTranslit Wordlists

--- tests/WordList/RuTranslitTest.php
+++ tests/WordList/RuTranslitTest.php
@@ -6,9 +6,27 @@ use Barzo\Password\WordList;
 
 class RuTranslitTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetLast()
+    public function testRuTranslit()
     {
         $wordList = new WordList\RuTranslit();
-        $this->assertEquals('ledi', $wordList->get(1));
+        $this->assertEquals('vodit', $wordList->get(1));
+    }
+
+    public function testRuTranslitAdjectives()
+    {
+        $wordList = new WordList\RuTranslit\Adjectives();
+        $this->assertEquals('ekstra', $wordList->get(1));
+    }
+    
+    public function testRuTranslitNouns()
+    {
+        $wordList = new WordList\RuTranslit\Nouns();
+        $this->assertEquals('kishka', $wordList->get(1));
+    }
+
+    public function testRuTranslitVerbs()
+    {
+        $wordList = new WordList\RuTranslit\Verbs();
+        $this->assertEquals('bulkat', $wordList->get(1));
     }
 }
